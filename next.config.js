1e53439aa345333e15b41d23a26814d6ff55eb4b
nstuder <niki.studer@web.de>
2025-06-09 17:24:05 +0200

    fix images on server

--- next.config.js
+++ next.config.js
@@ -9,17 +9,15 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
-        const url = new URL(item)
-
-        return {
-          hostname: url.hostname,
-          protocol: url.protocol.replace(':', ''),
-        }
-      }),
-    ],
-  },
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+                port: '',
+                pathname: '**',
+            },
+        ],
+    },
   output: 'standalone',
   reactStrictMode: true,
   redirects,
